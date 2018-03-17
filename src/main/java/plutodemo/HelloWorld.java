0dd13997a9d72b9c7425187ff5427c8e773507cc
mahendar <mahendar.sp5@gmail.com>
2018-03-17 06:39:25 +0530

    abc

---
+++ src/main/java/plutodemo/HelloWorld.java
@@ -0,0 +1,9 @@
+package plutodemo;
+
+public class HelloWorld {
+	public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 );
+	}
+
+}
---
+++ src/test/java/plutodemo/HelloWorldTest1.java
@@ -0,0 +1,14 @@
+package plutodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
